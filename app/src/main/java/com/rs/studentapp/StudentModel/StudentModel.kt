ec949f8b1b6f078e2dd1c486751ea985d122b9cd
Roei <roei159963@gmail.com>
2025-01-13 13:08:50 +0200

    13.1.25 after adding addStudent.

--- app/src/main/java/com/rs/studentapp/StudentModel/StudentModel.kt
+++ app/src/main/java/com/rs/studentapp/StudentModel/StudentModel.kt
@@ -1,11 +1,45 @@
 package com.rs.studentapp.StudentModel
 
-data class StudentModel(
+import android.os.Parcel
+import android.os.Parcelable
 
+data class StudentModel(
     var id: String,
     var name: String,
     var phoneNumber: String,
     var address: String,
     var avatarUrl: String,
     var isChecked: Boolean
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readByte() != 0.toByte()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeString(phoneNumber)
+        parcel.writeString(address)
+        parcel.writeString(avatarUrl)
+        parcel.writeByte(if (isChecked) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<StudentModel> {
+        override fun createFromParcel(parcel: Parcel): StudentModel {
+            return StudentModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<StudentModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
